f1d0334b1cdccbc4177d040b634c2848bd39d14d
Jennifer Kim <jjkim429@gmail.com>
2019-04-08 20:20:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,7 @@ def prime?(number)
   if num > 0 || num == 0 || num = 1
     return false
   else
-    
+    (2..number-1).to_a.all? do |possible_factor|
+      number % possible_factor != 0
+  end
 end
